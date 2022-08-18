f2b700e7a3476000da6a0d62b8e30c6cd60458ea
panda1 <blueblue0011@gmail.com>
2022-08-18 13:50:35 -0700

    created variables

---
+++ src/components/TheTweets.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return [{
+                tweet: `Your tweet here!`,
+                user:  `some_user`,
+                date: `Jun 1, 2021`
+            },
+            {
+                tweet: `Your tweet here too!`,
+                user:  `some_user_2`,
+                date: `Jun 2, 2021`
+            },
+            {
+                tweet: `Your tweet here too too!`,
+                user:  `some_user_3`,
+                date: `Jun 3, 2021`
+            },
+            {
+                tweet: `Your tweet here too now!`,
+                user:  `some_user_4`,
+                date: `Jun 4, 2021`
+            },
+            {
+                tweet: `Your tweet here next!`,
+                user:  `some_user_5`,
+                date: `Jun 5, 2021`
+            },
+            {
+                tweet: `Your tweet here next next!`,
+                user:  `some_user_6`,
+                date: `Jun 6, 2021`
+            },
+            {
+                tweet: `Your tweet here blah!`,
+                user:  `some_user_7`,
+                date: `Jun 7, 2021`
+            },
+            ]
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
