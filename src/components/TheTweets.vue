0079f743463891bc9b9c4043bff65bed3a32c9c1
panda1 <blueblue0011@gmail.com>
2022-08-21 21:31:41 -0700

    added comments to codes

--- src/components/TheTweets.vue
+++ src/components/TheTweets.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
+        <!-- using @ listening event for click to change boolean stored in data -->
         <button @click="change_on()">change view</button>
         <br><br>
+        <!-- vbind class that is linked to a boolean stored in data -->
         <section :class = "{section_container: isOn}">
+            <!-- had to change data grouping to give a name to array instead of blank to work -->
             <article class = "article_container" v-for = "(person, index) in list_of_people" :key="index" >
                 <p>{{list_of_people[index][`user`]}}</p>
                 <p>{{list_of_people[index][`tweet`]}}</p>
                 <p>{{list_of_people[index][`date`]}}</p>
             </article>
         </section>
+        <!-- for me to see {{}} pulling variable data that differs from v-for -->
         <p>{{this.$data[`list_of_people`][3][`tweet`]}}</p>
         <p>{{this.$data[`isOn`]}}
         
@@ -20,6 +24,7 @@
     export default {
         data: function() {
             return {
+                // data is in in object inside a array with a name instead of blank now
             list_of_people: [{
                 tweet: `Your tweet here!`,
                 user:  `some_user`,
@@ -56,6 +61,7 @@
                 date: `Jun 7, 2021`
             },
                 ],
+                // the boolean for the button to change view to work
                 isOn: true
             }
             
@@ -68,6 +74,7 @@
                     <br>date: ${this.$data[i][`date`]}</h3> </div`)
                 }
             },
+            // conditional to set true/false depending what the stored value is
             change_on: function() {
                 if (this.$data[`isOn`]) {
                this.$data[`isOn`] = false;
