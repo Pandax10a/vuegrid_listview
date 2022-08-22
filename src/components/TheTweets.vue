d487440a2354a08fbcb92994094646b45ed1f6d2
panda1 <blueblue0011@gmail.com>
2022-08-21 21:23:00 -0700

    finally learn to use boolean on class

--- src/components/TheTweets.vue
+++ src/components/TheTweets.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
-        <button onclick = "this.grid_this()">change view</button>
+        <button @click="change_on()">change view</button>
+        <br><br>
+        <section :class = "{section_container: isOn}">
+            <article class = "article_container" v-for = "(person, index) in list_of_people" :key="index" >
+                <p>{{list_of_people[index][`user`]}}</p>
+                <p>{{list_of_people[index][`tweet`]}}</p>
+                <p>{{list_of_people[index][`date`]}}</p>
+            </article>
+        </section>
+        <p>{{this.$data[`list_of_people`][3][`tweet`]}}</p>
+        <p>{{this.$data[`isOn`]}}
+        
         <h1>testing</h1>
     </div>
 </template>
@@ -8,7 +19,8 @@
 <script>
     export default {
         data: function() {
-            return [{
+            return {
+            list_of_people: [{
                 tweet: `Your tweet here!`,
                 user:  `some_user`,
                 date: `Jun 1, 2021`
@@ -43,23 +55,59 @@
                 user:  `some_user_7`,
                 date: `Jun 7, 2021`
             },
-            ]
+                ],
+                isOn: true
+            }
+            
         },
         methods: {
             list_all: function() {
                 
                 for (let i = 0; i<this.$data.length; i++){
-                    document.body.insertAdjacentHTML(`beforeend`, `<h3>tweet: ${this.$data[i][`tweet`]}</h3>`)
+                    document.this.$refs.toggle_change.insertAdjacentHTML(`beforeend`, `<div class = "change" <h3>user: ${this.$data[i][`user`]} <br>tweet: ${this.$data[i][`tweet`]}
+                    <br>date: ${this.$data[i][`date`]}</h3> </div`)
                 }
+            },
+            change_on: function() {
+                if (this.$data[`isOn`]) {
+               this.$data[`isOn`] = false;
+               
+                } else if (this.$data[`isOn`] === false) {
+                    this.$data[`isOn`] = true
+                    
+                }
+               
+               
             }
         },
         mounted () {
-            this.list_all();
+        //    this.list_all();
+            // document.body.querySelector(`#click_here`).addEventListener(`click`, this.grid_this)
+            // this.change_on()
         },
         name: "the-tweets"
     }
 </script>
 
 <style scoped>
+.section_container {
+    display: grid;
+    place-content: center;
+    grid-template-columns:1fr 1fr 1fr ;
+    row-gap: 20px;
+    
+}
+.article_container {
+    display: grid;
+    place-content: center;
+    background-color: grey;
+    box-shadow: 5px 5px 5px 5px blue;
+    
+    
+    
+    
+    
+    width: 30vw;
+}
 
 </style>
